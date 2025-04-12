62d883c9dd9a2c6945465ae8098b58aca65c4c46
Sivaram Challa <challa.sivaram.26@gmail.com>
2025-04-12 23:14:14 +0530

    keysearch

--- src/main/java/org/seleniumdemo/App.java
+++ src/main/java/org/seleniumdemo/App.java
@@ -2,18 +2,46 @@ package org.seleniumdemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.sql.Driver;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class App {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.python.org");
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("pycon");
-        searchBox.submit();
-        System.out.println("Title: " + driver.getTitle());
-        driver.quit();
+
+        ChromeOptions opts = new ChromeOptions();
+
+
+        String url = "https://www.google.com";
+        String keyword = "Gmail";
+        WebDriver driver = new ChromeDriver(opts);
+
+        try{
+            System.out.println("Opening " + url );
+            driver.get(url);
+            String bodytxt = driver.findElement(By.tagName("body")).getText();
+            if(bodytxt.toLowerCase().contains(keyword.toLowerCase()) ){
+                System.out.println("Keyword Found");
+            }
+            else{
+                System.out.println("keyword not found");
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error Occured " + e.getMessage());
+        }
+        finally{
+            try {
+                Thread.sleep(5000); // Wait 5 seconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            driver.quit();
+        }
+        
     }
 }
